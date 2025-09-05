12e65cd2ed6dbf3d757e40662e901eae1df6644d
Hallowdread <hallowpahe@gmail.com>
2025-09-05 13:53:38 +0100

    worked on the cart overview file

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,12 +1,18 @@
-import { Link } from "react-router";
+import { useSelector } from "react-redux";
 import LinkButton from "../../ui/LinkButton";
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
+import { formatCurrency } from "../../utils/helpers";
 
 function CartOverview() {
+  const totalCartQuantity = useSelector(getTotalCartQuantity);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+  // console.log(totalCartQuantity);
+  //
   return (
     <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm text-stone-200 uppercase sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+        <span>{totalCartQuantity} pizzas</span>
+        <span>{formatCurrency(totalCartPrice)}</span>
       </p>
       <LinkButton to="/cart">Open cart &rarr;</LinkButton>
     </div>
